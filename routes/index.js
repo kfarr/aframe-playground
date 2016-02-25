23b645ce25052dce4ce17cf637b1d8886182d3bd
kfarr <kieran.farr@gmail.com>
2016-02-24 22:49:50 -0800

    changed view engine to nunjucks
    Former-commit-id: 7142ebce12a304e03938ca07c0b43ab0a31435d3

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
 
 
-/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
-*/
+
 
 module.exports = router;
