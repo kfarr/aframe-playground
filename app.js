9d4df08bbe6466c1e560b11ee82e9e9266786ae1
kfarr <kieran.farr@gmail.com>
2016-03-05 21:29:23 -0800

    integrate aframe videosphere example from gtk2k
    Former-commit-id: 64815d50c5a262806c5deb520698e38c2e7d2f24

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var favicons = require('connect-favicons');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,15 +22,11 @@ nunjucks.configure('views', {
     express: app
 });
 
-
-
-
-
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use('/scripts/aframe', express.static(__dirname + './node_modules/aframe/'));
+
+app.use(favicons(__dirname + '/public/images/icons'));
 
 app.use('/scripts/aframe/dist', express.static(__dirname + '/node_modules/aframe/dist/'));
 
