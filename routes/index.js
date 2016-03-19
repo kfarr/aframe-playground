a462f0af82511c727026e00b4b2f525d3f2f7786
kieranfarr <kieran.farr@gmail.com>
2016-03-19 14:34:11 -0700

    A few cleanups and doc changes
    Former-commit-id: 0888bda021a6987e3d8195c51694c3086898eb64 [formerly 02ea8345c9a0dd2f118aedb24fdd4d5fd7dc10be]
    Former-commit-id: de5a6b351d462bc0819a75ea6b0b76e2b8c7b237

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page.
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-
 var pages = {
-  '1': {'name': '360 aframe iphone hack', url: '/360-mobile.html', description: '360° video playback using a-frame. Hack to make this work on iphone (for use with cardboard adapter). On iOS, you must save as an app on your homescreen before video object can be mapped to inner sphere texture with autoplay.'},
-  '2': {'name': '360 aframe no hack', url: '/360-aframe', description: '360° video playback using a-frame "native" video sphere.'},
-  '3': {'name': '360 bg still', url: '/360-still', description: '360° jpeg still using a-frame'},
-  '4': {'name': '360 bg still with video', url: '/360-still-video', description: '360° jpeg still with video in foreground'},
-  '5': {'name': '360 bg still with video iphone hack', url: '/360-still-video-hack', description: '360° jpeg still with video in foreground with iphone video hack'}
+  '1': {'name': '360 aframe iphone hack', url: '/360-mobile.html', description: '360° video playback using a-frame. This works on both iphone safari and android chrome, however on iOS, you must save as an app on your homescreen first (to allow for inline playback). Hack from https://github.com/gtk2k/gtk2k.github.io'},
+  '2': {'name': '360 aframe no hack', url: '/360-aframe', description: '360° video playback using a-frame "native" video sphere. This does not appear to work on either iOS nor Android Chrome '},
+  '3': {'name': '360 bg still', url: '/360-still', description: '360° jpeg still using a-frame, works on all devices.'},
+  '4': {'name': '360 bg still with video', url: '/360-still-video', description: '360° jpeg still with video in foreground, video does not play back on mobile.'},
+  '5': {'name': '360 bg still with video iphone hack', url: '/360-still-video-hack', description: '360° jpeg still with video in foreground with iphone video hack - work in progress to adapter gtk2k mobile hack to a non-360 video object (a plane with video texture)'}
 }
 
 var media = {
@@ -33,11 +27,9 @@ router.get('/360-aframe', function(req, res, next) {
 });
 
 router.get('/360-still', function(req, res, next) {
-
   function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-
   res.render('360-still.html', {media: media[getRandomInteger(3, 5).toString()]});
 });
 
